b5106eb8dd33cc8ee820cee269804f59489127ba
repavan <rps.zootecnista@gmail.com>
2025-09-01 11:21:44 -0300

    adicionada mensagem de erro na criação de um filme

--- src/server-aula02.ts
+++ src/server-aula02.ts
@@ -24,6 +24,25 @@ app.get('/', (req, res) => {
 });
 */
 
+app.post('/movies', async (req, res) => {
+   const { title, genre_id, language_id, oscar_count, release_date } = req.body;
+   try{
+      await prisma.movie.create({
+      data: {
+         title,
+         genre_id,
+         language_id,
+         oscar_count,
+         release_date: new Date(release_date),
+         }
+      });
+      }catch(error){
+         res.status(500).send({message: "Falha ao cadastrar um filme"});
+      } 
+
+   res.status(201).send();
+});
+
 app.listen(port, () => {
    console.log(`Servidor em execução em http://localhost:${port}`);
 });
